3e8d36fb0cc38cd782016aeb810475aad0bbabe8
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2022-04-16 00:56:52 +0900

    スキーマ構造の変更・許可ヘッダにカスタム認可ヘッダを追加

---
+++ src/domain/model/musics/OEmbedAPIResponse.ts
@@ -0,0 +1,14 @@
+export type OEmbedAPIResponse = {
+	version: string,
+	type: string,
+	provider_name: string,
+	provider_url: string,
+	height: string,
+	width: string,
+	title: string,
+	description: string,
+	thumbnail_url: string,
+	html: string,
+	author_name: string,
+	author_url: string
+}
